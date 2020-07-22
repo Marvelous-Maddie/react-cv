d7496c8ab8b3b3a421ab0e0cdc8a065ae4b99024
Madlen Engel <engel.madlen@googlemail.com>
2020-07-22 13:18:17 +0200

    Update Carousel: add pictures

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,27 +8,19 @@ const Carousel = () => {
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="../assets/0022_Engel_SW_2_21052019.jpg" class="d-block w-100" alt="Profile" />
+          <img src={require("../assets/Profile.jpg")} class="d-block w-100" alt="Profile" />
         </div>
         <div class="carousel-item">
-          <img src="https://cdn.pixabay.com/photo/2015/06/24/15/45/code-820275_960_720.jpg" class="d-block w-100" alt="Software Development" />
+          <img src={require("../assets/Software-Development.jpg")} class="d-block w-100" alt="Software Development" />
         </div>
         <div class="carousel-item">
-          <img src="https://images.pexels.com/photos/2681319/pexels-photo-2681319.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="Think about things differently" />
+          <img src={require("../assets/Project-Management.jpg")} class="d-block w-100" alt="Project Management" />
         </div>
         <div class="carousel-item">
-          <img src="https://images.pexels.com/photos/7376/startup-photos.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="d-block w-100" alt="Project Management" />
-        </div>
-        <div class="carousel-item">
-          <img src="" class="d-block w-100" alt="Software Development" />
-        </div>
-        <div class="carousel-item">
-          <img src="https://images.pexels.com/photos/1181396/pexels-photo-1181396.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="d-block w-100" alt="IT Training" />
+          <img src={require("../assets/IT-Training.jpg")} class="d-block w-100" alt="IT-Training" />
         </div>
       </div>
     </div>
