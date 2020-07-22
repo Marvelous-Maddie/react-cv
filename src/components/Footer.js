4f59c604699adf904d7d12e862a4b91fce2b8d57
Madlen Engel <engel.madlen@googlemail.com>
2020-07-22 13:18:43 +0200

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+    </nav>
+  )
+}
+
+export default Footer
